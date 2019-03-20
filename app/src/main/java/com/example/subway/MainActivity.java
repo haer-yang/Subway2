46e0827c8c0d31391a9ad241124b337b40e5850e
haer-yang <haerstudy0714@gmail.com>
2019-03-20 19:51:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/subway/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.subway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner sp =(Spinner)findViewById(R.id.sp);
+        ArrayAdapter spAdapter = ArrayAdapter.createFromResource(this,R.array.branch, )
+
+    }
+}
