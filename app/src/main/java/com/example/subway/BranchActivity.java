cd244c2f250b3ddcd36d5247d69560f9c81266fd
khaein <khaein1125@gmail.com>
2019-03-27 14:12:30 +0900

    main,branch activity 나눠놓음

---
+++ app/src/main/java/com/example/subway/BranchActivity.java
@@ -0,0 +1,20 @@
+package com.example.subway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class BranchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner sp =(Spinner)findViewById(R.id.sp);
+        ArrayAdapter spAdapter = ArrayAdapter.createFromResource(this,R.array.branch, android.R.layout.simple_spinner_dropdown_item );
+        sp.setAdapter(spAdapter);
+
+    }
+}
--- app/src/main/java/com/example/subway/MainActivity.java
+++ app/src/main/java/com/example/subway/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.subway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Spinner sp =(Spinner)findViewById(R.id.sp);
-        ArrayAdapter spAdapter = ArrayAdapter.createFromResource(this,R.array.branch, android.R.layout.simple_spinner_dropdown_item );
-        sp.setAdapter(spAdapter);
 
     }
 }
