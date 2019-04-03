a5a071f94ed07e7ad500a87ea28467692c66ed17
haer-yang <haerstudy0714@gmail.com>
2019-04-03 16:21:51 +0900

    0320-20시03분 라디오버튼, 체크박스버튼으로 메뉴 추가

--- app/src/main/java/com/example/subway/BranchActivity.java
+++ app/src/main/java/com/example/subway/BranchActivity.java
@@ -2,10 +2,17 @@ package com.example.subway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+
+
 public class BranchActivity extends AppCompatActivity {
+
+
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,31 @@ public class BranchActivity extends AppCompatActivity {
         ArrayAdapter spAdapter = ArrayAdapter.createFromResource(this,R.array.branch, android.R.layout.simple_spinner_dropdown_item );
         sp.setAdapter(spAdapter);
 
+
+
+
     }
+    View.OnClickListener next_click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        next =(Button)findViewById(R.id.next);
+        next.setOnClickListener(next_click);
+
+
+    }
+
+    
+
+    
+
+
 }
